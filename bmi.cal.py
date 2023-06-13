3ee07d2f77a680305936b518f7708f0bef0cc8e2
Hai Thanh Pham <59055154+selenpham@users.noreply.github.com>
2023-06-13 10:23:22 +0700

    Add files via upload

--- bmi.cal.py
+++ bmi.cal.py
@@ -6,14 +6,16 @@ sg.theme('DarkPurple4')
 
 # define the layout
 layout = [
-    [sg.Text('BMI Calculator', font=('Helvetica', 20,'bold'), size=(30, 1), justification='center')],
+    [sg.Text('BMI Calculator', font=('Helvetica', 20,'bold'), size=(30, 1), justification='center')], 
+    # [sg.Column([[sg.Text('BMI Calculator', font=('Helvetica', 20,'bold'), justification='center')]], element_justification='c')],
     [sg.Text('Chiều cao (cm) :', font=('Helvetica', 10)), sg.InputText(key='height')],
     [sg.Text('Cân nặng  (kg)  :', font=('Helvetica', 10)), sg.InputText(key='weight')],
     [sg.Button('Tính BMI', size=(11,1), button_color=('white','#730080'), font=('Helvetica', 11), pad=((8,0),(5,10)), border_width=2, focus=True), 
      sg.Button('Thoát', size=(6,1), button_color=('white','#730080'), font=('Helvetica', 11), pad=((0,8),(5,10)), border_width=2, focus=True)],
     [sg.Text('Chỉ số BMI : ', font=('Helvetica', 10)), sg.Text('0', font=('Helvetica', 10), size=(5,1), key='BMI',text_color='red')],
-    [sg.Text('Tình trạng   : ', font=('Helvetica', 10)), sg.Text('', font=('Helvetica', 10), key='category', text_color='red')]
-]
+    [sg.Text('Tình trạng   : ', font=('Helvetica', 10)), sg.Text('', font=('Helvetica', 10), key='category', text_color='red')],
+   [sg.Column([[sg.Text('@phamhaithanh', font=('Helvetica', 8))]], element_justification='right', pad=((400, 0), 0))]
+   ]
 
 # create the window
 window = sg.Window('BMI Calculator', layout)
@@ -42,7 +44,7 @@ while True:
             elif bmi >= 30.0:
                 window['category'].update('Bạn đang bị béo phì')
     except ValueError:
-        sg.popup("Please enter the correct value",font=("Helvetica",10))
+        sg.popup("Vui lòng nhập chỉ số của bạn!",font=("Helvetica",10))
 
 # close the window
 window.close()
